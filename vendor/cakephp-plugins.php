fc41a8fb920255e32356558c7a8faed74f7390a7
eddiePower <edster2007@gmail.com>
2015-05-06 11:13:47 +1000

    silly git ignore left out some files now all up to date

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,14 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'Bootstrap' => $baseDir . '/vendor/elboletaire/twbs-cake-plugin/',
+        'Bootstrap3' => $baseDir . '/vendor/holt59/cakephp3-bootstrap3-helpers/',
+        'BootstrapUI' => $baseDir . '/vendor/friendsofcake/bootstrap-ui/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Less' => $baseDir . '/vendor/elboletaire/less-cake-plugin/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Twit' => $baseDir . '/plugins/Twit/'
+    ]
+];
